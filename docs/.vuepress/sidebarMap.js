5c6ee4c8c7a6d7440b075a206a5eb1b03341a0a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-21 22:10:43 +0000

    Deploy xiaoxiaobingguai/notes to github.com/xiaoxiaobingguai/notes.git:master

--- docs/.vuepress/sidebarMap.js
+++
@@ -1,8 +0,0 @@
-const sidebarMap = [
-  { title: "文章", dirname: "article" },
-  { title: "面试", dirname: "interview" },
-  { title: "Blog", dirname: "blog" },
-  { title: "ABC", dirname: "abc"}
-]
-
-module.exports = sidebarMap
--- docs/README.md
+++
@@ -1,8 +0,0 @@
----
-home: true
-heroImage: /home.jpg
-actionText: 笔记 →
-actionLink: /repository/
-
-footer: MIT Licensed | Copyright © 2019-laiblame
----
